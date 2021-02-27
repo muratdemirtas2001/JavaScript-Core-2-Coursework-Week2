f37ae5710cfd3525363946203315d28af3747ea6
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-02-27 11:33:59 +0000

    completing B-shopping list

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,13 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
+function shoppingList(arrayOfShoppingList) {
+  let content = document.querySelector("#content");
+  let ulElement = document.createElement("ul");
+  content.appendChild(ulElement);
+  arrayOfShoppingList.forEach((item) => {
+    let liElement = document.createElement("li");
+    liElement.innerText = item;
+    ulElement.appendChild(liElement);
+  });
+  console.log(ulElement);
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
