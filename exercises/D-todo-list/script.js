b6f4803033008cd6af8be5b1bd322b5b7e780e81
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-02-27 12:18:44 +0000

    completing D-todo-list

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,12 +1,25 @@
 function todoList(todos) {
-  // Write your code here...
+  let content = document.querySelector("#content");
+  content.innerHTML = "My to do list for this week";
+  let ulElement = document.createElement("ul");
+  content.appendChild(ulElement);
+  todos.forEach((todoItem) => {
+    let liElement = document.createElement("li");
+    liElement.innerText = todoItem.todo;
+    ulElement.appendChild(liElement);
+    liElement.addEventListener("click", () => {
+      liElement.classList.toggle("line-through");
+    });
+  });
 }
 
 const todos = [
-  {todo: "wash the dishes"}, 
-  {todo: "walk the dog"}, 
-  {todo: "learn javascript"}, 
-  {todo: "go shopping"}
+  { todo: "wash the dishes" },
+  { todo: "walk the dog" },
+  { todo: "learn javascript" },
+  { todo: "go shopping" },
 ];
 
 todoList(todos);
+
+
