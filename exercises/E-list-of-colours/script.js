a2ecc13f51e0be6b127f4f6043f7f7424cec15d5
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-02-27 12:21:44 +0000

    completing E-list-of-colours

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,19 @@
 function listOfColours(colours) {
-  // Write your code here...
+  let content = document.querySelector("#content");
+  let selectElement = document.createElement("select");
+  content.appendChild(selectElement);
+  let pElement = document.createElement("p");
+  pElement.style.fontSize = "30px";
+  content.appendChild(pElement);
+  colours.forEach((color) => {
+    let colorElement = document.createElement("option");
+    colorElement.innerText = color;
+    selectElement.appendChild(colorElement);
+  });
+  selectElement.addEventListener("change", (event) => {
+    pElement.style.color = event.target.value;
+    pElement.innerText = `Hello you chose ${event.target.value} color`;
+  });
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
