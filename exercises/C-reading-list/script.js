f19dbd63f03ce7c62ecf23942c0086fdb3cece85
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-02-27 12:07:35 +0000

    c-reading-list

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,23 +1,46 @@
 function readingList(books) {
-  // Write your code here...
-}
+  const content = document.querySelector("#content");
+  content.innerText = "The Best Book of 2021";
+  let ulElement = document.createElement("ul");
+  content.appendChild(ulElement);
+  books.forEach((book) => {
+    const liElement = document.createElement("li");
+    ulElement.appendChild(liElement);
+    const pElement = document.createElement("p");
+    pElement.innerText = `${book.title}-${book.author}`;
+    liElement.appendChild(pElement);
+    const imageElement = document.createElement("img");
+    liElement.appendChild(imageElement);
+    imageElement.setAttribute("src", book.src);
+    //without using setAttribute
+    // imageElement.src= book.src;
 
+    if (book.alreadyRead) {
+      liElement.style.backgroundColor = "green";
+    } else {
+      liElement.style.backgroundColor = "red";
+    }
+  });
+}
 const books = [
   {
     title: "The Design of Everyday Things",
     author: "Don Norman",
-    alreadyRead: false
+    alreadyRead: false,
+    src: "1.jpg",
   },
   {
     title: "The Most Human Human",
     author: "Brian Christian",
-    alreadyRead: true
+    alreadyRead: true,
+    src: "2.jpg",
   },
   {
     title: "The Pragmatic Programmer",
     author: "Andrew Hunt",
-    alreadyRead: true
-  }
+    alreadyRead: true,
+    src: "3.jpg",
+  },
 ];
 
 readingList(books);
