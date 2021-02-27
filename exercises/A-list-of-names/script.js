75cedf724ff2ccd8eec9e5d178f0105b5c9bb13a
Murat Demirtas <muratdemirtas2001@gmail.com>
2021-02-27 11:22:19 +0000

    completing A-list-of-names

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,5 +1,13 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
+  arrayOfPeople.forEach((person) => {
+    let h1element = document.createElement("h1");
+    let h2element = document.createElement("h2");
+    h1element.innerText = person.name;
+    h2element.innerText = person.job;
+    content.appendChild(h1element);
+    content.appendChild(h2element);
+  });
 }
 
 let people = [
